dc9905d0bd2c78f84ca889b1563a29c4e3c9b942
AleixMJ <aleixtechIT@gmail.com>
2026-03-09 19:02:04 +0100

    added checkwin()
    This private function checks if rows, columns or diagonals match and
    returns a winner. Now the while loop for Draw() breaks once there is a
    winner. Now we need to return draw when all the slots are filled and also
    put controls to don't fill positions already filled by the other player.
    We are not adding controls for choosing positions that don't exist as
    that logic will change once we draw the board on the browser and draw is
    triggered by eventlisteners once a square is clicked.

--- index.js
+++ index.js
@@ -21,9 +21,33 @@ const tictactoe = (() => {
 
     const player1 = "X";
     const player2 = "O";
-    const winner = null;
+    let winner = null;
     let playerTurn = player1;
 
+    let checkWin = () => {
+
+        // rows
+        for (let i = 0 ; i < 3; i++) {            
+            if ( Gameboard[i][0] && Gameboard[i][0] == Gameboard[i][1] && Gameboard[i][0] == Gameboard[i][2]) {
+                return Gameboard[i][0];
+            }           
+        }
+         // columns
+        for (let i = 0 ; i < 3; i++) {            
+            if ( Gameboard[0][i] && Gameboard[0][i] == Gameboard[1][i] && Gameboard[0][i] == Gameboard[2][i]) {
+                return Gameboard[0][i];
+            }           
+        }
+         // diagonals
+         if ( Gameboard[0][0] && Gameboard[0][0] == Gameboard[1][1] && Gameboard[0][0] == Gameboard[2][2]) {
+            return Gameboard[0][0];
+         } else if ( Gameboard[2][0] && Gameboard[2][0] == Gameboard[1][1] && Gameboard[2][0] == Gameboard[0][2]) {
+            return Gameboard[2][0];
+         }
+
+        return null;
+    }
+    
     const Draw = () => {
         let positionX = prompt("Choose X position for drawing:");
         let positionY = prompt("Choose Y position for drawing:");
@@ -34,6 +58,7 @@ const tictactoe = (() => {
 
     while (winner == null) {
         Draw();
+        winner = checkWin();
     }
 
 })();
