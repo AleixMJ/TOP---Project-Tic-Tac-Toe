c5675edc1bb691e4c7ae905a663f2cfefdd75398
AleixMJ <aleixtechIT@gmail.com>
2026-03-09 10:48:01 +0100

    added some brainstorming comments for the project and create the Gameboard Constant

---
+++ index.js
@@ -0,0 +1,20 @@
+
+/* BRAINSTORMING
+
+Game logic:
+
+ start game function (
+        Create a 3x3 Grid as an array
+        Create 2 players
+        Create action to draw on grid
+        Create end game conditions
+)()
+
+*/
+
+const tictactoe = (() => {
+
+    const Gameboard = Array(3).fill().map(() => Array(3).fill(null));
+    
+
+})();
